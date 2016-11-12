9fe27ebc0459acac10f6079a78d77c05aa9d09ed
Damian Matyjaszek <damian@localhost.localdomain>
2016-11-12 21:16:14 +0100

    void

--- prog.cpp
+++ prog.cpp
@@ -2,11 +2,18 @@
 
 using namespace std;
 
+void drukuj(int x) {
+    for(int i=0; i<x; i++) cout << x-i << " ";
+    cout << endl;
+}
+
 int main() {
     int i=0;
 
     for(i=0; i<10; ++i) cout << i << " ";
     cout << endl;
 
+    drukuj(15);
+    
     return 0;
 }
