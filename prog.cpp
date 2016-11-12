1ce8675b6969b386c594bdb9a5219cbc87787dcb
Damian Matyjaszek <damian@localhost.localdomain>
2016-11-12 21:22:31 +0100

    dodaj

--- prog.cpp
+++ prog.cpp
@@ -7,11 +7,16 @@ void drukuj(int x) {
     cout << endl;
 }
 
+int dodaj(int a, int b) {
+    return a+b;
+}
+
 int main() {
     for(int i=0; i<10; ++i) cout << i << " ";
     cout << endl;
 
     drukuj(15);
+    cout << "dodaj(32,12) = "<< dodaj(32,12) << endl;
     
     return 0;
 }
